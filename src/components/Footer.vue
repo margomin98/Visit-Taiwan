53835dd839bb7401b515fa847a2d99330cfc04b1
margomin98 <68723858+margomin98@users.noreply.github.com>
2022-03-20 11:51:09 +0800

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -25,7 +25,7 @@
         </div>
     </div>
     <div class="container-fluid ">
-        <div class="row d-md-none d-xs-none">
+        <div class="row  d-xs-none">
             <div class="col social  d-flex justify-content-center">
                 <a href="https://www.facebook.com/">
                     <img src="../assets/facebook.png" alt="facebook"></a>
